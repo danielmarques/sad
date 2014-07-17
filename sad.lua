22887ca58b1caa7ad2444adf390253961f046a59
Daniel Marques <danielrosamarques@gmail.com>
2014-07-17 18:26:25 -0300

    Modificações do modulo principal sad.lua para usar o Dataentry.

--- sad.lua
+++ sad.lua
@@ -4,15 +4,35 @@
 ######################## Projeto de Programação 2014.1 #########################
 
 Autor: Daniel da Rosa Marques
-Data: 20/05/2014
+Data: 17/07/2014
 Arquivo: sad.lua
+Título: sad
 Descrição: Módulo principal do sistema. Deve ser executado via linha de comando 
 pelo usuário.
 
 ################################################################################
 --]]
+local Dataentry = require "Dataentry"
 
-print("Enter a number:")
+print("SAD - Sistema de Avaliação de Desempenho")
+print("Projeto de Programação 2014.1")
+print("\nAluno: Daniel da Rosa Marques")
+print("Orientador: Eduardo Sany Laber\n\n")
 
-a = io.read("*n")
-print("The number is: " .. a)
+print("Insira o caminho para o arquivo de instruções:")
+
+instFilePath = io.read()
+
+print("\nTentando ler o arquivo de instruções: " .. instFilePath .. "\n")
+
+local experiments = Dataentry.LoadExperiments(instFilePath)
+
+for experimentNum, experiment in pairs(experiments) do
+	print("\nExecutando o experimento número " .. experimentNum)
+
+	--print(experiment.data)
+
+	for tag, value in pairs(experiment) do
+		print(tag .. " = " .. value)
+	end
+end
